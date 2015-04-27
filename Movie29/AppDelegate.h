8455a7ee0ff0d924a1673db140052b03b55b26c7
alanchen <alan.chen@geniecapital.com>
2015-04-27 16:54:03 +0800

    Initial Commit

---
+++ Movie29/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movie29
+//
+//  Created by alan on 2015/4/27.
+//  Copyright (c) 2015年 alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Movie29/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Movie29
+//
+//  Created by alan on 2015/4/27.
+//  Copyright (c) 2015年 alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
