9f318c4f7bd1d4d98b06e9e65e12720974c25f56
alanchen <alan.chen@geniecapital.com>
2015-05-04 19:03:04 +0800

    add youtube api search parameter

--- Movie29/ViewController/DetailVC.m
+++ Movie29/ViewController/DetailVC.m
@@ -108,7 +108,7 @@
     [[YouTubeAPIService sharedInstance] apiSearchVideoDetaiilWithQuery:self.movieModel.title_cn
                                                             maxResults:30
                                                                  order:nil
-                                                                params:@"&videoEmbeddable=true&relevanceLanguage=zh-Hant"
+                                                                params:@"&videoEmbeddable=true&relevanceLanguage=zh-Hant&safeSearch=none"
                                                                success:^(NSMutableArray *results, id responseObject, id info) {
                                                                    [self.tableView.pullToRefreshView stopAnimating];
 
